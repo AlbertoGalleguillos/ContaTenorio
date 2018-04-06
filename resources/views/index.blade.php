24a92ca851b7cdcc832b1a27d3edbeab8b1456ee
Alberto Galleguillos <ai.galleguillosr@gmail.com>
2018-04-06 07:50:20 -0300

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/materialize.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Contabilidad Tenorio</title>
+</head>
+<body>
+
+<nav class="deep-purple">
+    <div class="container nav-wrapper">
+        <a href="#" class="brand-logo">Contabilidad Tenorio</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        </ul>
+    </div>
+</nav>
+
+<main>
+<div class="container">
+    <div class="row">
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title center">Emprende</span>
+                    <ul>
+                        <li><i class="material-icons">account_balance</i> Asesoría para crear tu empresa<li>
+                        <li><i class="material-icons">account_balance</i> Creación de Plan de Cuentas<li>
+                        <li><i class="material-icons">account_balance</i> Declaraciones Juradas<li>
+                        <li><i class="material-icons">account_balance</i> Inicio de Actividades Sii<li>
+                        <li><i class="material-icons">account_balance</i> Creación de Plan de Cuentas<li>
+                        
+                        <h4>Pago Único : 2 UF</h4>
+                    <ul>
+                </div>
+                <div class="card-action center">
+                    <a class="blue-text" href="#">Contratar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title center">Pyme</span>
+                    <ul>
+                        <li><i class="material-icons">account_balance</i> Optimización de Plan de Cuentas<li>
+                        <li><i class="material-icons">account_balance</i> DTE (Tope 50 documentos al mes)<li>
+                        <li><i class="material-icons">account_balance</i> Contabilidad General<li>
+                        <li><i class="material-icons">account_balance</i> Balance<li>
+                        <li><i class="material-icons">account_balance</i> Conciliación Bancaria (1 Ctacte.)<li>
+                        
+                        <h4>Pago Mensual : 1 UF</h4>
+                    <ul>
+                </div>
+                <div class="card-action center">
+                    <a class="blue-text" href="#">Contratar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title center">Empresarial</span>
+                    <ul>
+                        <li><i class="material-icons">account_balance</i> Optimización de Plan de Cuentas<li>
+                        <li><i class="material-icons">account_balance</i> Optimización de Centros de Costos<li>
+                        <li><i class="material-icons">account_balance</i> DTE Ilimitados<li>
+                        <li><i class="material-icons">account_balance</i> Contabilidad General, Tributaria y Financiera<li>
+                        <li><i class="material-icons">account_balance</i> Balance<li>
+                        <li><i class="material-icons">account_balance</i> Conciliación Bancaria para todas las cuentas que poseas<li>
+                        
+                        <h4>Contactanos para definir un presupuesto en base a los movimientos de tu compañía</h4>
+                    <ul>
+                </div>
+                <div class="card-action center">
+                    <a class="blue-text" href="#">Contratar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</main>
+
+        <footer class="page-footer deep-purple">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contabilidad Tenorio</h5>
+                <p class="grey-text text-lighten-4">Contabilidad para todos !</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            Desarrollador por
+            <a class="grey-text text-lighten-4" href="#!">Segasi</a>
+            </div>
+          </div>
+        </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+</body>
+</html>
